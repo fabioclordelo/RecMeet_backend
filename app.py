9f870016ee21a9c50561ebd7ae16dfd3943496db
Rodrigo Viana <rodrig0_jpa@hotmail.com>
2025-06-27 13:45:00 -0300

    new app.py

--- app.py
+++ app.py
@@ -176,6 +176,7 @@ def process():
         start = time.time()
 
         # Transcribe audio
+        print("📼 Starting chunked transcription...")
         raw_transcript, detected_langs = transcribe_audio(local_path)
         # Summarize transcript
         cleaned_transcript, summary = summarize_transcript(raw_transcript, detected_langs)
